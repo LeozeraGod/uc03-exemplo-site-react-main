59dfec5432955c4169e104c66ce390ef33336a1a
Leonardo Souza <leonardomellolopes@gmail.com>
2024-11-07 20:54:05 -0300

    Layout Incial

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,6 +1,6 @@
-import './App.css'
+import './styles.css'
 
-function App() {
+function Contato() {
     return (
         <div id="container">
         <nav id="menu">
@@ -16,4 +16,4 @@ function App() {
     )
 }
 
-export default App
+export default Contato

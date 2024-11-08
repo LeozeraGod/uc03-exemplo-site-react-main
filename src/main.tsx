c262c4284d9461282a476c4c8b5251e160a12b9b
Leonardo Souza <leonardomellolopes@gmail.com>
2024-11-07 21:57:54 -0300

    Commit do final da aula

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
 import './index.css'
-/*import App from './App.tsx'*/
+import App from './App.tsx'
 import Contato from './pages/Contato'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/contato',
+        element: <Contato />,
+    }
+
+])
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Contato />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
